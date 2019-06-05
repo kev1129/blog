f38bfe8ffa9e0ce29d7a9a13dea97417b265fc3c
kev1129 <shinado1129@gmail.com>
2019-06-05 19:54:51 +0900

    記事の取得とカテゴリー別の取得ができた

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,54 @@
 <template>
   <div>
+      <li v-for="post in blogTabs" :key="post">
+        title:{{ post.fields.blogTag }}
+        <nuxt-link :to="{ name: 'tags-id', params: { id: post.sys.id }}">
+          link
+        </nuxt-link>
+
+      </li>
+
     <nav>
         <ul>
             <li><nuxt-link to="posts">posts</nuxt-link></li>
             <li><nuxt-link to="profile">profile</nuxt-link></li>
-            <li><nuxt-link :to="{ name: 'tags-id', params: testTags }">tag: hi</nuxt-link></li>
+            <!-- <li><nuxt-link :to="{ name: 'tags-id', params: { tag: post.fields.blogTags } }">tag: hi</nuxt-link></li> -->
         </ul>
-        <!-- <ul v-for="tag in allTags" :key="tag">
-            <li><nuxt-link :to="{ name: 'tags-id', params: tag }">{{ tag }}</nuxt-link></li>
-        </ul> -->
-        <a href="tags/hi">hihi</a>
     </nav>
+      <li v-for="post in blogTabs" :key="post">
+        title:{{ post.fields.blogTag }}
+        <a href="tags/book">hi</a>
+        <nuxt-link :to="{ name: 'tags-slug', params: { slug: post.fields.blogTag }}">
+          Read Full Content
+        </nuxt-link>
+
+        <hr>
+      </li>
+      <div>
+        <h2>genre</h2>
+        <nuxt-link :to="{ name: 'tags-id', params: { id: 'hello' }}">
+          hello
+        </nuxt-link>
+        <nuxt-link :to="{ name: 'tags-id', params: { id: 'photo' }}">
+          photo
+        </nuxt-link>
+        <nuxt-link :to="{ name: 'tags-id', params: { id: 'book' }}">
+          book
+        </nuxt-link>
+        <nuxt-link :to="{ name: 'tags-id', params: { id: 'agri' }}">
+          agri
+        </nuxt-link>
+
+      </div>
     <!-- render data of the person -->
     <!-- render blog posts -->
-    <ul v-for="post in posts" :key="post">
-      <li>
-        tags:{{ post.fields.tags }}
-      </li>
-    </ul>
-    <hr>
     <ul>
       <li v-for="post in posts" :key="post">
         title:{{ post.fields.title }}
         <br>
         slug:{{ post.fields.slug }}
         <br>
-        tags:{{ post.fields.tags }}
+        tags:{{ post.fields.blogTags }}
         <br>
         <!-- <img v-bind:src="post.fields.image"/> -->
         desc:{{ post.fields.description }}
@@ -45,15 +68,11 @@
   import {createClient} from '~/plugins/contentful.js'
   const client = createClient()
   export default {
-    data () {
-      return {
-        testTags: 'javascript'
-      }
-    },
     // `env` is available in the context object
     asyncData ({env}) {
       return Promise.all([
         // fetch the owner of the blog
+        // client.getContentTypes(),
         client.getEntries({
           'sys.id': env.CTF_PERSON_ID
         }),
@@ -62,13 +81,21 @@
           'content_type': env.CTF_BLOG_POST_TYPE_ID,
           order: '-sys.createdAt'
         }),
-      ]).then(([entries, posts]) => {
+        client.getEntries({
+          content_type: 'blogTabs',
+          order: '-sys.createdAt'
+
+        })
+      ]).then(([entries, posts, blogTabs]) => {
         // return data that should be available
         // in the template
+          console.log(entries.items)
+          console.log(posts.items)
+          console.log(blogTabs.items)
         return {
-          person: entries.items[0],
+          person: entries.items,
           posts: posts.items,
-
+          blogTabs: blogTabs.items
         }
       }).catch(console.error)
     }
--- pages/tags/_id.vue
+++ pages/tags/_id.vue
@@ -6,18 +6,8 @@
     <ul>
       <li v-for="post in posts" :key="post">
         title:{{ post.fields.title }}
-        <br>
-        slug:{{ post.fields.slug }}
-        <br>
-        tags:{{ post.fields.tags }}
-        <br>
-        <!-- <img v-bind:src="post.fields.image"/> -->
-        desc:{{ post.fields.description }}
-        <br>
-        date:{{ ( new Date(post.fields.publishDate)).toDateString() }}
-        <br>
         <nuxt-link :to="{ name: 'posts-slug', params: { slug: post.fields.slug }}">
-          Read Full Content
+          link
         </nuxt-link>
         <hr>
       </li>
@@ -30,25 +20,29 @@
 
   const client = createClient()
 
+// fields.genre=hi というパラメータで取得するとこができた
+
   export default {
     // `env` is available in the context object
     asyncData ({env, params}) {
       return Promise.all([
         // fetch the owner of the blog
+        // client.getContentTypes(),
+        // fetch all blog posts sorted by creation date
         client.getEntries({
           'sys.id': env.CTF_PERSON_ID
         }),
-        // fetch all blog posts sorted by creation date
         client.getEntries({
           'content_type': env.CTF_BLOG_POST_TYPE_ID,
-          'fields.tags': params.tags,
-          order: '-sys.createdAt',
-        })
+          'fields.tags.sys.id': params.id,
+          order: '-sys.createdAt'
+        }),
       ]).then(([entries, posts]) => {
         // return data that should be available
         // in the template
         return {
-            post: posts.items[0],
+          preson: entries.items,
+          posts: posts.items,
         }
       }).catch(console.error)
     }
